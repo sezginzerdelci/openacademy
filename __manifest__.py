51229fbcd3919d15da199ae1294f8f098f128947
Sezgin Zerdelci <sezginzerdelci@gmail.com>
2019-08-26 10:16:43 +0300

    Openacademy modüle dosyaları eklendi.

---
+++ __manifest__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Open Academy",
+
+    'summary': """Manage trainings""",
+
+    'description': """
+        Open Academy module for managing trainings:
+            - training courses
+            - training sessions
+            - attendees registration
+    """,
+
+    'author': "Sezgin ZERDELCI",
+    'website': "http://www.yourcompany.com",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Test',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base'],
+
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/templates.xml',
+        'views/openacademy.xml',
+        'views/partner.xml',
+        'reports.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo.xml',
+    ],
+}
